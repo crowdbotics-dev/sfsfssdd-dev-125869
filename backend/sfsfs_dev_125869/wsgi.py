c1fcda49df5306351927dbbb03a8441bd665263a
Crowdbotics <engineering@crowdbotics.com>
2023-10-31 06:33:03 +0000

    Initial commit

---
+++ backend/sfsfs_dev_125869/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for sfsfs_dev_125869 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "sfsfs_dev_125869.settings"
+)
+
+application = get_wsgi_application()
